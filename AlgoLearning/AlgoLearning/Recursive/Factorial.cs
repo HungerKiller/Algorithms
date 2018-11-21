c210d3301ea41b5c0fe719aa67ebb6069a10caa7
HungerKiller <zhongshui.wang.chn@gmail.com>
2018-11-21 21:14:21 +0800

    Add factorial code and test

---
+++ AlgoLearning/AlgoLearning/Recursive/Factorial.cs
@@ -0,0 +1,13 @@
+﻿namespace AlgoLearning.Recursive
+{
+    public class Factorial
+    {
+        public static int CalculateFactorial(int n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return n * CalculateFactorial(n - 1);
+        }
+    }
+}
---
+++ AlgoLearning/XUnitTestAlgoLearning/UnitTest.cs
@@ -0,0 +1,19 @@
+using AlgoLearning.Recursive;
+using Xunit;
+
+namespace XUnitTestAlgoLearning
+{
+    public class UnitTest
+    {
+        [Theory]
+        [InlineData(3, 6)]
+        [InlineData(4, 24)]
+        [InlineData(5, 120)]
+        [InlineData(6, 720)]
+        public void Test1(int value, int expected)
+        {
+            int actual = Factorial.CalculateFactorial(value);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
