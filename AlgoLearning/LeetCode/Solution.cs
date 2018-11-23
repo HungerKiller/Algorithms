40e3f1c985d0fa17c2822943deb274d08c68b82a
HungerKiller <zhongshui.wang.chn@gmail.com>
2018-11-23 18:18:11 +0800

    p3

--- AlgoLearning/LeetCode/Solution.cs
+++ AlgoLearning/LeetCode/Solution.cs
@@ -1,21 +1,119 @@
-﻿namespace LeetCode
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
 {
     public class Solution
     {
+        #region Problem 1
+
+        // Basic
+        public static int[] TwoSum1(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                        return new int[] { i, j };
+                }
+            }
+            return null;
+        }
+
+        // Have issue "Memory Limit Exceeded"
+        public static int[] TwoSum2(int[] nums, int target)
+        {
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                int num2 = target - nums[i];
+                nums = nums.Take(nums.Length - 1).ToArray(); // Take need more memory
+                if (nums.Contains(num2))
+                    return new int[] { nums.ToList().IndexOf(num2), i };
+            }
+            return null;
+        }
+
+        // OK
         public static int[] TwoSum(int[] nums, int target)
         {
-            int[] result = new int[2];
-            result[0] = 0;
-            foreach (var num in nums)
+            Dictionary<int, int> indexAndNum = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
             {
-                for (result[1] = result[0] + 1; result[1] < nums.Length; result[1]++)
-                {
-                    if (nums[result[1]] == target - num)
-                        return result;
-                }
-                result[0]++;
+                int num2 = target - nums[i];
+                if (indexAndNum.ContainsValue(num2))
+                    return new int[] { indexAndNum.FirstOrDefault(item => item.Value == num2).Key, i };
+                indexAndNum.Add(i, nums[i]);
             }
-            return result;
+            return null;
         }
+
+        #endregion Problem 1
+
+        #region Problem 2
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            if (l1 == null && l2 == null)
+                return null;
+            if (l1 == null)
+                l1 = new ListNode(0);
+            if (l2 == null)
+                l2 = new ListNode(0);
+            ListNode listNode = new ListNode(l1.val + l2.val);
+            if (listNode.val >= 10)
+            {
+                listNode.val -= 10;
+                if (l1.next == null)
+                    l1.next = new ListNode(1);
+                else
+                    l1.next.val++;
+            }
+            listNode.next = AddTwoNumbers(l1.next, l2.next);
+            return listNode;
+        }
+
+        #endregion Problem 2
+
+        #region Problem 3
+
+        public static int LengthOfLongestSubstring(string s)
+        {
+            int length = 0;
+            List<char> charList = new List<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                for(int j = i; j < s.Length; j++)
+                {
+                    if (charList.Contains(s[j]))
+                    {
+                        length = (length < charList.Count()) ? charList.Count() : length;
+                        charList.Clear();
+                        break;
+                    }
+                    charList.Add(s[j]);
+                }
+            }
+            length = (length < charList.Count()) ? charList.Count() : length;
+            return length;
+        }
+
+        #endregion Problem 3
+
+        #region Problem 4
+
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            return 0;
+        }
+
+        #endregion Problem 4
     }
 }
--- AlgoLearning/XUnitTestLeetCode/UnitTest.cs
+++ AlgoLearning/XUnitTestLeetCode/UnitTest.cs
@@ -17,7 +17,22 @@ namespace XUnitTestLeetCode
         public static IEnumerable<object[]> Data => new List<object[]>
         {
             new object[] { new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 } },
-            new object[] { new int[] { -1, -2, -3, -4, -5 }, -8, new int[] { 2, 4 } }
+            new object[] { new int[] { -1, -2, -3, -4, -5 }, -8, new int[] { 2, 4 } },
+            new object[] { new int[] { 3, 3 }, 6, new int[] { 0, 1 } }            
         };
+
+        [Theory]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        [InlineData(" ", 1)]
+        [InlineData("aab", 2)]
+        [InlineData("dvdf", 3)]
+        public void TestLengthOfLongestSubstring(string value, int expected)
+        {
+            var actual = Solution.LengthOfLongestSubstring(value);
+            Assert.Equal(expected, actual);
+        }
+        
     }
 }
